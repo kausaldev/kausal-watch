c972a4a3d3487fc0732f7f7e022c09efae254aec
Timo Tuominen <tituomin@gmail.com>
2024-06-28 13:33:33 +0300

    Whitespace cleanup

--- indicators/models.py
+++ indicators/models.py
@@ -310,7 +310,7 @@ class IndicatorQuerySet(SearchableQuerySetMixin, models.QuerySet):
         if user is None or not user.is_authenticated:
             return self.filter(visibility=RestrictedVisibilityModel.VisibilityState.PUBLIC)
         return self
-    
+
     def visible_for_public(self) -> Self:
         return self.visible_for_user(None)
 
@@ -629,7 +629,7 @@ class Indicator(ClusterableModel, index.Indexed, ModificationTracking, PlanDefau
         if (user is None or not user.is_authenticated) and self.visibility != RestrictedVisibilityModel.VisibilityState.PUBLIC:
             return False
         return True
-    
+
     def is_visible_for_public(self) -> Self:
         return self.is_visible_for_user(None)
 
@@ -757,7 +757,7 @@ class IndicatorLevelQuerySet(SearchableQuerySetMixin, models.QuerySet):
         if user is None or not user.is_authenticated:
             return self.filter(indicator__visibility=RestrictedVisibilityModel.VisibilityState.PUBLIC)
         return self
-    
+
     def visible_for_public(self) -> Self:
         return self.visible_for_user(None)
 
@@ -912,7 +912,7 @@ class ActionIndicatorQuerySet(models.QuerySet):
         if user is None or not user.is_authenticated:
             return self.filter(indicator__visibility=RestrictedVisibilityModel.VisibilityState.PUBLIC)
         return self
-    
+
     def visible_for_public(self) -> Self:
         return self.visible_for_user(None)
 
